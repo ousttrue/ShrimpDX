c783be4796b75c2a65c5b6a092975410fa11c589
ousttrue <ousttrue@gmail.com>
2019-05-21 19:28:56 +0900

    WIP D2DSample

---
+++ D2DSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace D2DSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
