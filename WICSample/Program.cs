df34716aac62bd655e667936777bb0803b968ad5
ousttrue <ousttrue@gmail.com>
2019-05-22 09:08:12 +0900

    WIP WICSample

---
+++ WICSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WICSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
