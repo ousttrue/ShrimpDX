51ccc426c208651d999ee932b33d9f043a0403ab
ousttrue <ousttrue@gmail.com>
2019-06-29 02:22:10 +0900

    PinPtr ArraySegment

--- ComPtrCS/ComPtrCS/Pin.cs
+++ ComPtrCS/ComPtrCS/Pin.cs
@@ -5,25 +5,43 @@ namespace ComPtrCS
 {
     public struct PinPtr<T> : IDisposable
     {
+        int m_offset;
         readonly GCHandle m_handle;
-        public IntPtr Ptr => m_handle.AddrOfPinnedObject();
+        public IntPtr Ptr
+        {
+            get
+            {
+                if (!m_handle.IsAllocated)
+                {
+                    return IntPtr.Zero;
+                }
+                var ptr = m_handle.AddrOfPinnedObject();
+                ptr = IntPtr.Add(ptr, m_offset);
+                return ptr;
+            }
+        }
 
         public void Dispose()
         {
             m_handle.Free();
         }
 
-        public PinPtr(T[] array)
+        public PinPtr(ArraySegment<T> segment)
         {
-            m_handle = GCHandle.Alloc(array, GCHandleType.Pinned);
+            m_handle = GCHandle.Alloc(segment.Array, GCHandleType.Pinned);
+            m_offset = segment.Offset;
         }
     }
 
     public static class PinPtr
     {
-        public static PinPtr<T> Create<T>(T[] array)
+        public static PinPtr<T> Create<T>(ArraySegment<T> array)
         {
             return new PinPtr<T>(array);
+        }
+        public static PinPtr<T> Create<T>(T[] array)
+        {
+            return Create(new ArraySegment<T>(array));
         }
     }
 
